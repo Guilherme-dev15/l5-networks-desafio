ed433f72d0d4bd230b28e715f6b90d875894b989
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2025-09-02 17:25:50 -0300

    Criado a estrutura do projeto

---
+++ src/app/features/episodes/episodes-list/episodes-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-episodes-list',
+  imports: [],
+  templateUrl: './episodes-list.html',
+  styleUrl: './episodes-list.scss'
+})
+export class EpisodesList {
+
+}
